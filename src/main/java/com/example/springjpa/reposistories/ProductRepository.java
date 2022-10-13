d179752103a87acc6d59851760577b205a9f3625
Daniel K I Ikeuchi <daniel.ikeuchi@gmail.com>
2022-10-13 18:55:48 -0300

    Product

---
+++ src/main/java/com/example/springjpa/reposistories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.springjpa.reposistories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.springjpa.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
